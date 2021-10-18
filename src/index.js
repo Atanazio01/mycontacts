857ce6d51f48671157773eace031df62b7ee8288
Atanazio01 <marcosd.atanazio@gmail.com>
2021-10-18 18:40:19 -0300

    Creating registration user route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const routes = require('./routes');
 
 const app = express();
+
+app.use(express.json()); // Pegar o body
+
 app.use(routes);
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));

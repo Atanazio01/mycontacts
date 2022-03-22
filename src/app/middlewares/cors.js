67a2863e65a981748c800e2cd9c45040b1905fa3
atanazio <atanazio@huhsp.org.br>
2022-03-22 15:35:47 -0300

    Simple vs Preflighted requests

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,7 @@
 module.exports = (request, response, next) => {
   // Wildcard -> Curinga -> *
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
   next();
 };

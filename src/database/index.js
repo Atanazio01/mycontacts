8dd540224a88a4f659fe9184d27c2e7dbc4da24a
Atanazio01 <marcosd.atanazio@gmail.com>
2022-04-03 14:35:35 -0300

    Utilizing mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -15,3 +15,46 @@ exports.query = async (query, values) => {
   console.log(rows);
   return rows;
 };
+
+// const { MongoClient } = require('mongodb');
+// require('dotenv/config');
+// // or as an es module:
+// // import { MongoClient } from 'mongodb'
+// const {
+//   DB_MONGO_HOST,
+//   DB_MONGO_HOST_PORT,
+//   DB_MONGO_AUTH_USER,
+//   DB_MONGO_AUTH_USER_PASSWORD,
+//   DB_MONGO_AUTH_DATABASE,
+// } = process.env;
+
+// // Connection URL
+// const url = `mongodb://${DB_MONGO_AUTH_USER}:${DB_MONGO_AUTH_USER_PASSWORD}@${DB_MONGO_HOST}:${DB_MONGO_HOST_PORT}/${DB_MONGO_AUTH_DATABASE}`;
+// const client = new MongoClient(url);
+
+// // Database Name
+// const dbName = 'patients';
+
+// async function main() {
+//   // Use connect method to connect to the server
+//   await client.connect();
+
+//   console.log('Connected successfully to server');
+
+//   const db = await client.db(dbName);
+
+//   const collection = await db.collection('auth').find().toArray();
+
+//   const code = collection.map((item) => (
+//     item.code
+//   ));
+
+//   // the following code examples can be pasted here...
+
+//   return code;
+// }
+
+// main()
+//   .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close());
